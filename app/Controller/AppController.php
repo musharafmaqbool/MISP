6e488572c7cfe25c69f4d15e234904634521a1a2
Sami Mokaddem <mokaddem.sami@gmail.com>
2025-03-26 16:48:25 +0100

    fix: [workflow:editor] Improved caching of ajax calls for select_options_url and chosen behavior with after-draw callback
    - Potential fix for #10256

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '171';
+    private $__queryVersion = '172';
     public $pyMispVersion = '2.5.7';
     public $phpmin = '8.1';
     public $phprec = '8.2';
