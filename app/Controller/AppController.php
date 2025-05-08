3ed38cbf223710f89159f79b8e52ba1684b0c3e3
ThomasLcr <thomaslacroix54@hotmail.com>
2025-05-08 09:28:28 +0200

    add: [Warninglists] Split Known Identifier from False Positive in the event view

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '174';
+    private $__queryVersion = '175';
     public $pyMispVersion = '2.5.10';
     public $phpmin = '7.2';
     public $phprec = '7.4';
