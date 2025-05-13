38612aff7a8c46aaa8d0193a396f838badffb42f
ThomasLcr <thomaslacroix54@hotmail.com>
2025-05-13 15:08:50 +0200

    fix: [API] support preference of type

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1399,17 +1399,46 @@ class AppController extends Controller
         );
         $exception = false;
         $filters = $this->_harvestParameters($filterData, $exception, $this->_legacyParams);
-        $acceptHeader = $this->request->header('Accept');
         if (empty($filters) && $this->request->is('get')) {
             throw new BadRequestException(__('Restsearch queries using GET and no parameters are not allowed. If you have passed parameters via a JSON body, make sure you use POST requests.'));
         }
+
         if (empty($filters['returnFormat'])) {
-            if (preg_match('#application/([a-zA-Z0-9\-\+]+)#', $acceptHeader, $matches)) {
-                $format = strtolower(trim($matches[1]));
-                $filters['returnFormat'] = $format;
-            } else {
-                $filters['returnFormat'] = 'json';
+            $acceptHeader = $this->request->header('Accept');
+            $acceptedTypesRaw = explode(',', $acceptHeader);
+
+            //Array to hold the mime types and their quality values
+            $mimeQuality = [];
+
+            foreach ($acceptedTypesRaw as $entry) {
+                $parts = explode(';', trim($entry));
+                $mime = trim($parts[0]);
+                $q = 1.0; 
+
+                foreach ($parts as $part) {
+                    if (strpos($part, 'q=') !== false) {
+                        $q = (float) str_replace('q=', '', trim($part));
+                        break;
+                    }
+                }
+
+                if (strpos($mime, '/') !== false) {
+                    list(, $subtype) = explode('/', $mime, 2);
+                    $format = strtolower(trim($subtype));
+                    // Verify if the format is valid for the model of the request
+                    foreach ($this->$modelName->validFormats as $key => $value) {
+                        if ($key === $format) {
+                            $mimeQuality[$format] = $q;
+                            break;
+                        }
+                    }
+                }
             }
+            // Descending sort the array by value
+            arsort($mimeQuality);
+            // Take the first key of the array
+            $returnFormat = key($mimeQuality);
+            $filters['returnFormat'] = $returnFormat ?: 'json';
         }
         unset($filterData);
         if ($filters === false) {
