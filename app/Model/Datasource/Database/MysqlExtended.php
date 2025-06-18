33a774d0e1042303a985ceefbde3ea11ff44f5f6
iglocska <andras.iklody@gmail.com>
2025-06-18 12:01:47 +0200

    fix: [database handler] fixed

--- app/Model/Datasource/Database/MysqlExtended.php
+++ app/Model/Datasource/Database/MysqlExtended.php
@@ -24,6 +24,38 @@ class MysqlExtended extends Mysql
     ];
 
     /**
+     * Builds and generates a JOIN condition from an array. Handles final clean-up before conversion.
+     *
+     * @param array $join An array defining a JOIN condition in a query.
+     * @return string An SQL JOIN condition to be used in a query.
+     * @see DboSource::renderJoinStatement()
+     * @see DboSource::buildStatement()
+     */
+    public function buildJoinStatement($join, $reversed_alias = null, $reversed_table = null) {
+        $data = array_merge(array(
+            'type' => null,
+            'alias' => null,
+            'table' => 'join_table',
+            'conditions' => '',
+        ), $join);
+
+        if (!empty($reversed_alias)) {
+            $data['alias'] = $this->name($reversed_alias);
+        } else if (!empty($data['alias'])) {
+            $data['alias'] = $this->alias . $this->name($data['alias']);
+        }
+        if (!empty($data['conditions'])) {
+            $data['conditions'] = trim($this->conditions($data['conditions'], true, false));
+        }
+        if (!empty($reversed_table)) {
+            $data['table'] = $reversed_table;
+		} else if (!empty($data['table']) && (!is_string($data['table']) || strpos($data['table'], '(') !== 0)) {
+			$data['table'] = $this->fullTableName($data['table']);
+		}
+		return $this->renderJoinStatement($data);
+	}
+
+    /**
      * Output SHA1 as binary, that is faster and uses less memory
      * @param string $value
      * @return string
