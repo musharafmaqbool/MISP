425c1fbeb969194e0ef7bf5bb6da11cedb753cf7
Luciano Righetti <luciano.righetti@gmail.com>
2025-07-23 10:15:31 +0200

    fix: missing acl

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -86,12 +86,12 @@ class TasksController extends AppController
             throw new MethodNotAllowedException('You are not authorised to do that.');
         }
 
-        $this->set('dropdownData', $this->getDropdownData());
+        $this->set('dropdownData', $this->__getDropdownData());
 
         if ($this->request->is('post')) {
 
             $this->request->data['Task']['message'] = '';
-            $this->request->data = $this->massageFormInput($this->request->data);
+            $this->request->data = $this->__massageFormInput($this->request->data);
 
             $this->CRUD->add();
             if ($this->restResponsePayload) {
@@ -111,10 +111,10 @@ class TasksController extends AppController
             throw new MethodNotAllowedException('You are not authorised to do that.');
         }
 
-        $this->set('dropdownData', $this->getDropdownData());
+        $this->set('dropdownData', $this->__getDropdownData());
 
         if ($this->request->is('put')) {
-            $this->request->data = $this->massageFormInput($this->request->data);
+            $this->request->data = $this->__massageFormInput($this->request->data);
             $this->CRUD->edit($id);
             if ($this->restResponsePayload) {
                 return $this->restResponsePayload;
@@ -184,119 +184,7 @@ class TasksController extends AppController
         }
     }
 
-    private function getDropdownData()
-    {
-        $this->Server = ClassRegistry::init('Server');
-        $this->Feed = ClassRegistry::init('Feed');
-        $this->Workflow = ClassRegistry::init('Workflow');
 
-        $workflows = $this->Workflow->find('all', [
-            'order' => ['Workflow.name' => 'ASC'],
-        ]);
-
-        // Filter enabled workflows to only include those that have ad-hoc triggers
-        $dropdownWorkflows = [];
-        foreach ($workflows as $workflow) {
-            if ($workflow['Workflow']['enabled']) {
-                foreach ($workflow['Workflow']['listening_triggers'] as $listeningTrigger) {
-                    if ($listeningTrigger['is_adhoc']) {
-                        $dropdownWorkflows[$workflow['Workflow']['id']] = $workflow['Workflow']['name'];
-                        break;
-                    }
-                }
-            }
-        }
-
-        $dropdownData = [
-            'users' => $this->User->find('list', [
-                'fields' => ['User.id', 'User.email'],
-                'conditions' => ['User.disabled' => 0],
-                'order' => ['User.email' => 'ASC']
-            ]),
-            'servers' =>  ['all' => __('All Servers')] + $this->Server->find('list', [
-                'fields' => ['Server.id', 'Server.name'],
-                'order' => ['Server.name' => 'ASC']
-            ]),
-            'feeds' => ['all' => __('All Feeds')] + $this->Feed->find('list', [
-                'fields' => ['Feed.id', 'Feed.name'],
-                'order' => ['Feed.name' => 'ASC']
-            ]),
-            'workflows' => $dropdownWorkflows,
-        ];
-
-        return $dropdownData;
-    }
-
-    private function massageFormInput(array $data)
-    {
-        if ($data['Task']['type'] === 'Server') {
-            $data['Task']['action'] = $data['Task']['server_action'];
-            $data['Task']['params'] = implode(
-                ',',
-                [
-                    $data['Task']['server_id'],
-                    $data['Task']['server_technique']
-                ]
-            );
-        } elseif ($data['Task']['type'] === 'Feed') {
-            $data['Task']['action'] = $data['Task']['feed_action'];
-
-            if ($data['Task']['feed_action'] === 'fetch') {
-                if (!isset($data['Task']['feed_id']) || empty($data['Task']['feed_id'])) {
-                    $this->Flash->error(__('Please select a feed.'));
-                    return;
-                }
-                $data['Task']['params'] = $data['Task']['feed_id'];
-            } elseif ($data['Task']['feed_action'] === 'cache') {
-                if (!isset($data['Task']['feed_scope']) || empty($data['Task']['feed_scope'])) {
-                    $this->Flash->error(__('Please select a feed scope.'));
-                    return;
-                }
-                if ($data['Task']['feed_id'] === 'all') {
-
-                    $data['Task']['params'] = implode(
-                        ',',
-                        [
-                            $data['Task']['feed_id'],
-                            $data['Task']['feed_scope'],
-                        ]
-                    );
-                } else {
-                    $data['Task']['params'] = $data['Task']['feed_id'];
-                }
-            } else {
-                $this->Flash->error(__('Invalid action for Feed'));
-                return;
-            }
-        } elseif ($data['Task']['type'] === 'Workflow') {
-            $data['Task']['action'] = 'execute';
-
-            if (!isset($data['Task']['workflow']) || empty($data['Task']['workflow'])) {
-                $this->Flash->error(__('Please select a workflow.'));
-                return;
-            }
-            $data['Task']['params'] = $data['Task']['workflow'];
-        } else {
-            $this->Flash->error(__('Invalid type'));
-            return;
-        }
-
-        $data['Task']['timer'] = $data['Task']['time_multiplier'] * $data['Task']['time_unit'];
-
-        if ($data['Task']['timer'] < 60) {
-            $this->Flash->error(__('Invalid timer value, must be at least 60 seconds or 1 minute.'));
-            return;
-        }
-
-        if ($data['Task']['next_execution_date']) {
-            $time = $data['Task']['next_execution_time'] == "" ? '00:00:00' : $data['Task']['next_execution_time'];
-            $data['Task']['next_execution_time'] = strtotime($data['Task']['next_execution_date'] . ' ' . $time);
-        } else {
-            $data['Task']['next_execution_time'] = time() - 1;
-        }
-
-        return $data;
-    }
 
     public function forceRun($id)
     {
@@ -411,5 +299,119 @@ class TasksController extends AppController
             'error' => $output ?? $backtrace[0] ?? '',
             'backtrace' => $backtrace
         ];
+    }
+
+    private function __getDropdownData()
+    {
+        $this->Server = ClassRegistry::init('Server');
+        $this->Feed = ClassRegistry::init('Feed');
+        $this->Workflow = ClassRegistry::init('Workflow');
+
+        $workflows = $this->Workflow->find('all', [
+            'order' => ['Workflow.name' => 'ASC'],
+        ]);
+
+        // Filter enabled workflows to only include those that have ad-hoc triggers
+        $dropdownWorkflows = [];
+        foreach ($workflows as $workflow) {
+            if ($workflow['Workflow']['enabled']) {
+                foreach ($workflow['Workflow']['listening_triggers'] as $listeningTrigger) {
+                    if ($listeningTrigger['is_adhoc']) {
+                        $dropdownWorkflows[$workflow['Workflow']['id']] = $workflow['Workflow']['name'];
+                        break;
+                    }
+                }
+            }
+        }
+
+        $dropdownData = [
+            'users' => $this->User->find('list', [
+                'fields' => ['User.id', 'User.email'],
+                'conditions' => ['User.disabled' => 0],
+                'order' => ['User.email' => 'ASC']
+            ]),
+            'servers' =>  ['all' => __('All Servers')] + $this->Server->find('list', [
+                'fields' => ['Server.id', 'Server.name'],
+                'order' => ['Server.name' => 'ASC']
+            ]),
+            'feeds' => ['all' => __('All Feeds')] + $this->Feed->find('list', [
+                'fields' => ['Feed.id', 'Feed.name'],
+                'order' => ['Feed.name' => 'ASC']
+            ]),
+            'workflows' => $dropdownWorkflows,
+        ];
+
+        return $dropdownData;
+    }
+
+    private function __massageFormInput(array $data)
+    {
+        if ($data['Task']['type'] === 'Server') {
+            $data['Task']['action'] = $data['Task']['server_action'];
+            $data['Task']['params'] = implode(
+                ',',
+                [
+                    $data['Task']['server_id'],
+                    $data['Task']['server_technique']
+                ]
+            );
+        } elseif ($data['Task']['type'] === 'Feed') {
+            $data['Task']['action'] = $data['Task']['feed_action'];
+
+            if ($data['Task']['feed_action'] === 'fetch') {
+                if (!isset($data['Task']['feed_id']) || empty($data['Task']['feed_id'])) {
+                    $this->Flash->error(__('Please select a feed.'));
+                    return;
+                }
+                $data['Task']['params'] = $data['Task']['feed_id'];
+            } elseif ($data['Task']['feed_action'] === 'cache') {
+                if (!isset($data['Task']['feed_scope']) || empty($data['Task']['feed_scope'])) {
+                    $this->Flash->error(__('Please select a feed scope.'));
+                    return;
+                }
+                if ($data['Task']['feed_id'] === 'all') {
+
+                    $data['Task']['params'] = implode(
+                        ',',
+                        [
+                            $data['Task']['feed_id'],
+                            $data['Task']['feed_scope'],
+                        ]
+                    );
+                } else {
+                    $data['Task']['params'] = $data['Task']['feed_id'];
+                }
+            } else {
+                $this->Flash->error(__('Invalid action for Feed'));
+                return;
+            }
+        } elseif ($data['Task']['type'] === 'Workflow') {
+            $data['Task']['action'] = 'execute';
+
+            if (!isset($data['Task']['workflow']) || empty($data['Task']['workflow'])) {
+                $this->Flash->error(__('Please select a workflow.'));
+                return;
+            }
+            $data['Task']['params'] = $data['Task']['workflow'];
+        } else {
+            $this->Flash->error(__('Invalid type'));
+            return;
+        }
+
+        $data['Task']['timer'] = $data['Task']['time_multiplier'] * $data['Task']['time_unit'];
+
+        if ($data['Task']['timer'] < 60) {
+            $this->Flash->error(__('Invalid timer value, must be at least 60 seconds or 1 minute.'));
+            return;
+        }
+
+        if ($data['Task']['next_execution_date']) {
+            $time = $data['Task']['next_execution_time'] == "" ? '00:00:00' : $data['Task']['next_execution_time'];
+            $data['Task']['next_execution_time'] = strtotime($data['Task']['next_execution_date'] . ' ' . $time);
+        } else {
+            $data['Task']['next_execution_time'] = time() - 1;
+        }
+
+        return $data;
     }
 }
